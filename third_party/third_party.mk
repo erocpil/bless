b8670b87c87b5f4065b52faa1ce625a06365f011
Your Name <you@example.com>
2026-01-16 18:31:23 +0800

    add third_party/third_party.mk

---
+++ third_party/third_party.mk
@@ -0,0 +1,4 @@
+# auto generated, do not edit
+THIRD_PARTY_CFLAGS := -I/root/src/bless/third_party/libyaml/upstream/include -I/root/src/bless/third_party/cjson/upstream/. -I/root/src/bless/third_party/civetweb/upstream/include
+THIRD_PARTY_LDFLAGS := -L/root/src/bless/lib
+THIRD_PARTY_LDLIBS := -lyaml -lcjson -lcivetweb
