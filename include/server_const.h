72d0f410a7dda3b927eaf906f57fce393f206559
Your Name <you@example.com>
2026-02-06 20:31:11 +0800

    add server_const.{c,h}

---
+++ include/server_const.h
@@ -0,0 +1,10 @@
+#ifndef __SERVER_CONST_H__
+#define __SERVER_CONST_H__
+
+extern const unsigned char index_html[];
+extern const unsigned int index_html_len;
+
+extern const unsigned char config_yaml[];
+extern const unsigned int config_yaml_len;
+
+#endif
